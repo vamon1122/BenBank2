827bd02acce24b178c29da7fcb9b7cacace76b36
vamon1122 <35330025+vamon1122@users.noreply.github.com>
2019-05-29 15:23:35 +0100

    Improved transaction logging on MainWindow

--- BenBank2/MainWindow.xaml.cs
+++ BenBank2/MainWindow.xaml.cs
@@ -71,6 +71,8 @@ namespace BenBank2
 
                 Debug.WriteLine(string.Format("Payee \"{0}\" was selected. It has {1}", ((UserControl_FinancialEntity)ListBox_Payees.SelectedItem).MyFinancialEntity.Name, ((UserControl_FinancialEntity)ListBox_Payees.SelectedItem).MyFinancialEntity.Balance.ToString("£0.00")));
 
+                int NoOfTransactions = ListBox_Payers.SelectedItems.Count * ListBox_Payees.SelectedItems.Count;
+                int TransactionNo = 0;
                 PayerNo = 0;
                 PayeeNo = 0;
                 foreach (UserControl_FinancialEntity payer in ListBox_Payers.SelectedItems)
@@ -78,19 +80,22 @@ namespace BenBank2
                     PayerNo++;
                     foreach (UserControl_FinancialEntity payee in ListBox_Payees.SelectedItems)
                     {
+                        TransactionNo++;
                         PayeeNo++;
                         Transaction transaction = new Transaction();
                         transaction.Sender = payer.MyFinancialEntity;
                         transaction.Recipient = payee.MyFinancialEntity;
                         transaction.Ammount = Ammount;
+
+                        double SenderStartingBalance = transaction.Sender.Balance;
+                        double RecipientStartingBalance = transaction.Recipient.Balance;
+
                         transaction.DoTransaction();
-                        Debug.WriteLine(string.Format("Payer {0}/{1} \"{2}\" payed payee {3}/{4} \"{5}\" {6}. {2} now has {7}", PayerNo, ListBox_Payers.SelectedItems.Count, transaction.Sender.Name, PayeeNo, ListBox_Payees.SelectedItems.Count ,transaction.Recipient.Name, Ammount.ToString("£0.00"), transaction.Sender.Balance.ToString("£0.00")));
+                        Debug.WriteLine(string.Format("Transaction {0}/{1} - Payer {2}/{3} \"{4}\" payed payee {5}/{6} \"{7}\" {8}. {4} started with {9} but now has {10}. {7} started with {11} but now has {12}.", TransactionNo, NoOfTransactions, PayerNo, ListBox_Payers.SelectedItems.Count, transaction.Sender.Name, PayeeNo, ListBox_Payees.SelectedItems.Count ,transaction.Recipient.Name, Ammount.ToString("£0.00"), SenderStartingBalance.ToString("£0.00"), transaction.Sender.Balance.ToString("£0.00"), RecipientStartingBalance.ToString("£0.00"), transaction.Recipient.Balance.ToString("£0.00")));
                     }
                     PayeeNo = 0;
-                    
                 }
                 RefreshFinancialEntities();
-
             }
 
             bool ValidateTextBoxAmmount()
